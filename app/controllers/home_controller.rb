7b45c64057856b00440af80c0a5494f12587758e
Kapil Patidar <kpatidar@systematixtechnocrates.com>
2011-06-03 21:10:28 +0530

    commit after server side validation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,17 @@
 
 class HomeController < ApplicationController
-    #layout "admin_layout"
   def login
   end
+
   def logout
     session[:current_user] = nil unless session[:current_user].nil?
     redirect_to :action => 'login'
   end
+
   def check_login
     login = params[:login]
-    #pass = params[:password]
-    pass = get_encryp_pass(params[:password])
-    @user = User.find(:first,:conditions=>[" login_name = ? and login_password = ? ",login,pass] )
+    password = get_encryp_pass(params[:password])
+    @user = User.find(:first,:conditions=>[" login_name = ? and login_password = ? ",login,password] )
     if @user 
       if @user.login_role =="admin" then
 	  session[:current_user] = @user
@@ -21,10 +21,6 @@ class HomeController < ApplicationController
 	  session[:current_user] = user.id
 	  flash[:note]="Welcome employee " + @user.login_name
 	  redirect_to :controller =>'users', :action => 'employee'
-	  #elsif(user.login_role=="") then
-	  #      redirect_to :action => 'login'
-	  #else
-	    #  redirect_to :action => 'login'
       end 
     else
       redirect_to :action => 'login'
