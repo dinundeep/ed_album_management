c9db984af5a6993519b24b61eba7e6dc4207f1c4
Kapil Patidar <kpatidar@systematixtechnocrates.com>
2011-06-17 11:33:21 +0530

    Commit after all module complete

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,6 @@
 class Album < ActiveRecord::Base
   belongs_to :user
+  has_many :photos ,:dependent => :destroy
+
+  
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,11 @@
 class Photo < ActiveRecord::Base
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => '/images/admin.jpeg'
   belongs_to :album
+  has_many :comments
+  has_many :users, :through => :comments
+
+  def is_owner?(user)
+    album =  user.albums.find(:first,:conditions=>[" id = ? " ,self.album.id]) 
+    album ? true : false 
+  end
 end
